310cefb94ae16995b78bba5a09d9decf642b6724
mingming715 <richkang715@gmail.com>
2021-12-13 20:40:58 +0900

    20211213

--- hi.py
+++ hi.py
@@ -2,5 +2,13 @@ print('hello')
 print('hi')
 
 class MingBabo:
-    for x in range(5):
-        print('ming babo')
+
+    def __init__(self, name):
+        self.name = name
+
+    def SayBabo(name):
+        for x in range(5):
+            print(name+'babo')
+name='ming'
+ming = MingBabo('ming')
+ming.SayBabo('ming')

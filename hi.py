5531a7c93d54f74567406cdc3b9633cba6a565bb
mingming715 <richkang715@gmail.com>
2021-12-15 21:31:12 +0900

    211215 21:31 Changed

--- hi.py
+++ hi.py
@@ -1,21 +1,25 @@
-# ###print('hello')
-# print('hi')
+print('hello')
+print('hi')
 
-# class MingBabo:
+class MingBabo:
 
-#     def __init__(self, name):
-#         self.Name = name
+   def __init__(self, name):
+      self.Name = name
 
-#     def SayBabo(self, name):
-#         for x in range(5):
-#             print(name +'babo')
+   def SayBabo(self, name):
+      for x in range(5):
+            print(name +'babo')
             
-# name='ming'
-# ming = MingBabo('ming')
-# ming.SayBabo('민석')
+name='ming'
+ming = MingBabo('ming')
+ming.SayBabo('민석')
 
 list = [1, 2, 3, 4, 5]
 
 for a in enumerate(list):
- #   print(type(a))
-    print('{} : {}'.format(a[0], a[1]))
+   print(type(a))
+   print('{} : {}'.format(*a))
+
+a = 3/0
+
+print("0으로 나눌 수 없습니다.")

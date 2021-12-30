3d43ac49dad7c0d255a392610eb1a42a97936029
mingming715 <richkang715@gmail.com>
2021-12-30 17:52:33 +0900

    211230 17:52 Changed

--- hi.py
+++ hi.py
@@ -1,25 +1,41 @@
-print('hello')
-print('hi')
+import sys
+input = sys.stdin.readline()
 
-class MingBabo:
+sum=0
+num, base = map(int, input.split(' '))
+num_list = str(num)
+num_list_rev = num_list[::-1]
+for i in range(len(num_list)):
+   sum += int(num_list_rev[i]) * (base**i)
+   
+print(sum)
 
-   def __init__(self, name):
-      self.Name = name
 
-   def SayBabo(self, name):
-      for x in range(5):
-            print(name +'babo')
+
+
+
+# print('hello')
+# print('hi')
+
+# class MingBabo:
+
+#    def __init__(self, name):
+#       self.Name = name
+
+#    def SayBabo(self, name):
+#       for x in range(5):
+#             print(name +'babo')
             
-name='ming'
-ming = MingBabo('ming')
-ming.SayBabo('민석')
+# name='ming'
+# ming = MingBabo('ming')
+# ming.SayBabo('민석')
 
-list = [1, 2, 3, 4, 5]
+# list = [1, 2, 3, 4, 5]
 
-for a in enumerate(list):
-   print(type(a))
-   print('{} : {}'.format(*a))
+# for a in enumerate(list):
+#    print(type(a))
+#    print('{} : {}'.format(*a))
 
-a = 3/0
+# a = 3/0
 
-print("0으로 나눌 수 없습니다.")
+# print("0으로 나눌 수 없습니다.")
